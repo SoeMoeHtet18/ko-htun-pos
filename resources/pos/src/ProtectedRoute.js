2f6d3b8efbb57443e4006691d9ce4ba3fe7f18aa
Nay Myo Mhan <naymyomhan@gmail.com>
2024-02-27 11:23:33 +0700

    add:public

--- resources/pos/src/ProtectedRoute.js
+++ resources/pos/src/ProtectedRoute.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {Tokens} from "./constants";
-import {Navigate} from "react-router-dom";
+import { Tokens } from "./constants";
+import { Navigate } from "react-router-dom";
 
-const  ProtectedRoute = (props) => {
-    const {component} = props
+const ProtectedRoute = (props) => {
+    const { component } = props
     const token = localStorage.getItem(Tokens.ADMIN)
     if (!token) {
         return <Navigate to='/login' replace={true} />;
     }
-    return  component;
+    return component;
 };
 
 export default ProtectedRoute;
